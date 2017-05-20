c71ad104ff02d2f3323500a0d01f845ec3ab63a1
Axel GabrielMolaro <axelmolaro3@gmail.com>
2017-05-20 18:27:49 -0300

    agregre la herramienta

---
+++ scawOld/src/main/java/ar/edu/unlam/diit/scaw/beans/heramientas/HerramientasExprecionesRegulares.java
@@ -0,0 +1,25 @@
+package ar.edu.unlam.diit.scaw.beans.heramientas;
+import java.lang.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class HerramientasExprecionesRegulares
+{  
+    public static Boolean tieneComillasElString(String stringAVer)
+    {
+        //Expresion regular, 1er caracter si o si letra, resto alfanumerico
+        String REGEX = "^[a-zA-Z0-9]+$";;
+        
+        //Crea el patron a comparar (supongo)
+        Pattern pattern = Pattern.compile(REGEX);
+
+        String inputAnalizado1  = stringAVer;//String a analizar
+        
+        //En base a esto se puede comparar con 3 metodos. Solo usado matches()
+        Matcher matcher1 = pattern.matcher(inputAnalizado1);
+        return matcher1.matches();
+     
+        
+       
+    }
+}
